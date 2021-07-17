e3a1b718bcc25c290fb36671734b7a4b2f2b1b43
thanapatk <22460@gmail.com>
2021-07-17 17:00:43 +0700

    added image upload to storage

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import numpy as np
 import cv2
 import dlib
-import concurrent.futures # threading
-
+from firebase_admin import credentials, initialize_app
 import pyrealsense2 as rs
+import concurrent.futures # threading
+from datetime import datetime
 
 # Local file
 from utils.face_recognition.face import startDeepFace, recognition
 from utils.liveness.liveness import validate_face
-from utils.firestore.firestore import update
+from utils.firebase.firestore import FirestoreClient
 
-def face_recog(db_path, model_name, distance_metric, face_frame=5):
+def main(db_path, model_name, distance_metric, face_frame=5):
 	"""Main function to run this program"""
 
 	#region predefined
-	identity = None
+	identity = None; raw_image = None
 	have_face = False
 	current_faces = []
 	face_included_frames = 0
@@ -22,6 +23,17 @@ def face_recog(db_path, model_name, distance_metric, face_frame=5):
 	text_color = (255, 255, 255)
 
 	size = (640,480)
+	#endregion
+
+	#region Firebase setup
+	cert = 'config/scan-face.json'
+
+	cred = credentials.Certificate(cert=cert)
+	app = initialize_app(cred, {
+		'storageBucket': 'scan-face-49afb.appspot.com'
+	})
+
+	firestore = FirestoreClient(app)
 	#endregion
 
 	# Initialize the model
@@ -54,14 +66,18 @@ def face_recog(db_path, model_name, distance_metric, face_frame=5):
 	#endregion
 
 	#region threading
-	executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
+	executor = concurrent.futures.ThreadPoolExecutor(max_workers=4)
 
 	def callback(future):
 		"Callback function for threading"
-		nonlocal identity
-		identity, time = future.result()
-		print(identity)
-		executor.submit(update, identity)
+
+		nonlocal identity, raw_image
+		output, time = future.result()
+		now = datetime.utcnow()
+
+		identity = {'identity': output, 'time': now}
+		firestore.update(output, now, raw_image)
+		#executor.submit(update, output, now, raw_image)
 	#endregion
 
 	try:
@@ -79,6 +95,7 @@ def face_recog(db_path, model_name, distance_metric, face_frame=5):
 				continue
 
 			color_image = np.asanyarray(color_frame.get_data())
+			raw_image = color_image.copy()
 			
 			# Face Detection
 			faces = detector(color_image)
@@ -88,31 +105,32 @@ def face_recog(db_path, model_name, distance_metric, face_frame=5):
 				have_face = False
 				current_faces = []
 				identity = None
-
-			for box in faces:
+			else:
+				box = faces[0]
 				l,r,t,b = box.left(), box.right(), box.top(), box.bottom()
 				if r - l > 130:
 					# Liveness detection
 					landmark = predictor(color_image, box)
 					is_real_face = validate_face(depth_frame, depth_scale, landmark)
-					_box_color = is_real_face and (0,255,0) or (0,0,255) 
 
+					# Draw box around image
+					_box_color = is_real_face and (0,255,0) or (0,0,255) 
 					cv2.rectangle(color_image, (l,t), (r,b), _box_color, 1)
 
 					if is_real_face and face_included_frames < face_frame:
-						identity = 'Collecting...'
 						detected_face = color_image[t:b, l:r]
 
 						current_faces.append(detected_face)
 						face_included_frames += 1
 					
 					if identity != None:
-						cv2.putText(color_image, identity, (l,t), cv2.FONT_HERSHEY_SIMPLEX, 0.5, text_color, 1)
+						#TODO: Add UI to indicate user sign in
+						cv2.putText(color_image, identity['identity'], (l,t), cv2.FONT_HERSHEY_SIMPLEX, 0.5, text_color, 1)
 
+			# Face recognition
 			if face_included_frames >= face_frame and not have_face:
 				future = executor.submit(recognition, current_faces)
 				future.add_done_callback(callback)
-				identity = 'Identifying...'
 				have_face = True 
 
 			cv2.imshow('frame', color_image)
@@ -124,4 +142,4 @@ def face_recog(db_path, model_name, distance_metric, face_frame=5):
 		pipeline.stop()
 	
 if __name__ == '__main__':
-	face_recog('db', 'VGG-Face', 'cosine', face_frame=10)
+	main('db', 'VGG-Face', 'cosine', face_frame=10)
