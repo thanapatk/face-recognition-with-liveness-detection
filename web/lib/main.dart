d07a9511f05cbb24bbba4b7d6e615d31d1a49bef
thanapatk <22460@gmail.com>
2021-07-29 14:16:06 +0700

    added Theme to constant

--- web/lib/main.dart
+++ web/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:web/screens/wrapper.dart';
 import 'package:web/services/auth.dart';
-import 'package:web/shared/hex_color.dart';
+import 'package:web/shared/constant.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 
@@ -28,62 +28,7 @@ class MyApp extends StatelessWidget {
             create: (context) => _authService.isSignIn, initialData: null)
       ],
       child: MaterialApp(
-        theme: ThemeData(
-          brightness: Brightness.light,
-          backgroundColor: HexColor('f8f8f8'),
-          colorScheme: ColorScheme(
-            primary: HexColor("#28c76f"),
-            primaryVariant: HexColor("#1f9d57"),
-            secondary: HexColor('#7367F0'),
-            secondaryVariant: HexColor('#9b93f5'),
-            surface: HexColor('#ffffff'),
-            background: HexColor('#f8f8f8'),
-            error: Colors.red,
-            onPrimary: Colors.white,
-            onSecondary: Colors.white,
-            onSurface: Colors.grey.shade400,
-            onBackground: Colors.black,
-            onError: Colors.white,
-            brightness: Brightness.light,
-          ),
-          fontFamily: 'Kanit',
-          textTheme: TextTheme(
-            headline1: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: HexColor("#101010"),
-            ),
-            headline2: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: HexColor("#101010"),
-            ),
-            headline3: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: HexColor("#101010"),
-            ),
-            headline4: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: HexColor("#101010"),
-            ),
-            headline5: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: HexColor("#101010"),
-            ),
-            headline6: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: HexColor("#101010"),
-            ),
-            bodyText1: TextStyle(
-              fontWeight: FontWeight.normal,
-              color: HexColor("#101010"),
-              fontSize: 20,
-            ),
-            bodyText2: TextStyle(
-              fontWeight: FontWeight.normal,
-              color: HexColor("#101010"),
-              fontSize: 18,
-            ),
-          ),
-        ),
+        theme: psmTheme,
         home: const Wrapper(),
       ),
     );
--- web/lib/shared/constant.dart
+++ web/lib/shared/constant.dart
@@ -1,16 +1,74 @@
 import 'package:flutter/material.dart';
 import 'hex_color.dart';
 
+ThemeData psmTheme = ThemeData(
+  brightness: Brightness.light,
+  backgroundColor: HexColor('f8f8f8'),
+  colorScheme: ColorScheme(
+    primary: HexColor("#28c76f"),
+    primaryVariant: HexColor("#1f9d57"),
+    secondary: HexColor('#7367F0'),
+    secondaryVariant: HexColor('#9b93f5'),
+    surface: HexColor('#ffffff'),
+    background: HexColor('#f8f8f8'),
+    error: Colors.red,
+    onPrimary: Colors.white,
+    onSecondary: Colors.white,
+    onSurface: Colors.grey.shade400,
+    onBackground: Colors.black,
+    onError: Colors.white,
+    brightness: Brightness.light,
+  ),
+  hintColor: Colors.grey.shade400,
+  fontFamily: 'Kanit',
+  textTheme: TextTheme(
+    headline1: TextStyle(
+      fontWeight: FontWeight.bold,
+      color: HexColor("#101010"),
+    ),
+    headline2: TextStyle(
+      fontWeight: FontWeight.bold,
+      color: HexColor("#101010"),
+    ),
+    headline3: TextStyle(
+      fontWeight: FontWeight.bold,
+      color: HexColor("#101010"),
+    ),
+    headline4: TextStyle(
+      fontWeight: FontWeight.bold,
+      color: HexColor("#101010"),
+    ),
+    headline5: TextStyle(
+      fontWeight: FontWeight.bold,
+      color: HexColor("#101010"),
+    ),
+    headline6: TextStyle(
+      fontWeight: FontWeight.bold,
+      color: HexColor("#101010"),
+    ),
+    bodyText1: TextStyle(
+      fontWeight: FontWeight.normal,
+      color: HexColor("#101010"),
+      fontSize: 20,
+    ),
+    bodyText2: TextStyle(
+      fontWeight: FontWeight.normal,
+      color: HexColor("#101010"),
+      fontSize: 18,
+    ),
+  ),
+);
+
 InputDecoration textInputDecoration = InputDecoration(
-  fillColor: Colors.white,
+  fillColor: psmTheme.colorScheme.surface,
   filled: true,
   enabledBorder: OutlineInputBorder(
     borderRadius: BorderRadius.circular(10),
-    borderSide: BorderSide(color: Colors.grey.shade400, width: 1),
+    borderSide: BorderSide(color: psmTheme.hintColor, width: 1),
   ),
   focusedBorder: OutlineInputBorder(
     borderRadius: BorderRadius.circular(10),
-    borderSide: BorderSide(color: HexColor('#28c76f'), width: 2),
+    borderSide: BorderSide(color: psmTheme.colorScheme.primary, width: 2),
   ),
   errorBorder: OutlineInputBorder(
     borderRadius: BorderRadius.circular(10),
@@ -18,8 +76,8 @@ InputDecoration textInputDecoration = InputDecoration(
   ),
   focusedErrorBorder: OutlineInputBorder(
     borderRadius: BorderRadius.circular(10),
-    borderSide: BorderSide(color: HexColor('#28c76f'), width: 2),
+    borderSide: BorderSide(color: psmTheme.colorScheme.primary, width: 2),
   ),
   hintStyle: TextStyle(
-      fontWeight: FontWeight.normal, color: Colors.grey.shade400, fontSize: 20),
+      fontWeight: FontWeight.normal, color: psmTheme.hintColor, fontSize: 20),
 );
