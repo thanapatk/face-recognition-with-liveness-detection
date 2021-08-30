baaf9b4df408fca6fcc92feafe3e1eb89cc55edb
thanapatk <22460@gmail.com>
2021-08-30 21:37:27 +0700

    added animation

--- python/main.py
+++ python/main.py
@@ -1,15 +1,21 @@
 import numpy as np
 import cv2
 import dlib
+import time
 from firebase_admin import credentials, initialize_app
 import pyrealsense2 as rs
 import concurrent.futures # threading
 from datetime import datetime, timezone
 
 # Local file
+from utils.user_card import addUserCard, cornerRadius
 from utils.face_recognition.face import startDeepFace, recognition
 from utils.liveness.liveness import validate_face
 from utils.firebase.firestore import FirestoreClient
+
+def check_face(box, img) -> bool:
+	shape = img.shape
+	return box.left() > 0 and box.right() < shape[1] and box.top() > 0 and box.bottom() < shape[0]
 
 def main(db_path, model_name, distance_metric, face_frame=5):
 	"""Main function to run this program"""
@@ -19,8 +25,33 @@ def main(db_path, model_name, distance_metric, face_frame=5):
 	have_face = False
 	current_faces = []
 	face_included_frames = 0
+
+	bg_color = (255, 255, 255)
+	fg_color = (0,0,0)
+
+	card_top_left = (100, 350)
+	card_bottom_right = (540, 480)
+
+	alpha_animate_frame = 3
+	card_animate_frame = 5
+	fps = 30
 	
-	text_color = (255, 255, 255)
+	max_alpha = 1
+	min_alpha = .57
+	alpha = max_alpha
+	alpha_animate_amount = (max_alpha - min_alpha) / alpha_animate_frame
+
+	card_max = card_bottom_right[1]
+	card_min = card_top_left[1]
+	card_height = card_max
+	card_animate_amount = (card_max - card_min) / card_animate_frame
+	
+	font_path = 'fonts/Kanit-Light.ttf'
+	font_size = 24
+
+	# set the delay between each scan (in seconds)
+	delay = 5
+	prev_time = 0
 
 	size = (640,480)
 	#endregion
@@ -29,9 +60,7 @@ def main(db_path, model_name, distance_metric, face_frame=5):
 	cert = 'config/scan-face.json'
 
 	cred = credentials.Certificate(cert=cert)
-	app = initialize_app(cred, {
-		'storageBucket': 'scan-face-49afb.appspot.com'
-	})
+	app = initialize_app(cred, {'storageBucket': 'scan-face-49afb.appspot.com'})
 
 	firestore = FirestoreClient(app)
 	#endregion
@@ -44,8 +73,8 @@ def main(db_path, model_name, distance_metric, face_frame=5):
 	config = rs.config()
 
 	# Enabling realsense cam
-	config.enable_stream(rs.stream.color, size[0], size[1], rs.format.bgr8, 30)
-	config.enable_stream(rs.stream.depth, size[0], size[1], rs.format.z16, 30)
+	config.enable_stream(rs.stream.color, size[0], size[1], rs.format.bgr8, fps)
+	config.enable_stream(rs.stream.depth, size[0], size[1], rs.format.z16, fps)
 	profile = pipeline.start(config)
 
 	depth_sensor = profile.get_device().first_depth_sensor()
@@ -53,7 +82,7 @@ def main(db_path, model_name, distance_metric, face_frame=5):
 
 	# Configure color sensor
 	color_sensor.set_option(rs.option.backlight_compensation, 1)
-	color_sensor.set_option(rs.option.gamma, 400)
+	color_sensor.set_option(rs.option.gamma, 500)
 
 	# Getting the depth sensor's depth scale
 	depth_scale = depth_sensor.get_depth_scale()
@@ -73,11 +102,11 @@ def main(db_path, model_name, distance_metric, face_frame=5):
 
 		nonlocal identity, raw_image
 		output, time = future.result()
-		now = datetime.now(tz=timezone.utc)
-
-		identity = {'identity': output, 'time': now}
+		utc_now = datetime.now(tz=timezone.utc)
+		
+		identity = {'identity': output, 'time': utc_now.astimezone()}
 		#firestore.update(output, now, raw_image)
-		executor.submit(lambda p: firestore.update(*p), [output, now, raw_image])
+		#executor.submit(lambda p: firestore.update(*p), [output, now, raw_image])
 	#endregion
 
 	try:
@@ -100,15 +129,13 @@ def main(db_path, model_name, distance_metric, face_frame=5):
 			# Face Detection
 			faces = detector(color_image)
 
-			if len(faces) != 1:
-				face_included_frames = 0
-				have_face = False
-				current_faces = []
-				identity = None
-			else:
+			can_scan = time.time() - prev_time > delay
+
+			if len(faces) == 1 and can_scan:
 				box = faces[0]
+				shape = color_image.shape
 				l,r,t,b = box.left(), box.right(), box.top(), box.bottom()
-				if r - l > 130:
+				if r - l > 130 and check_face(box, color_image):
 					# Liveness detection
 					landmark = predictor(color_image, box)
 					is_real_face = validate_face(depth_frame, depth_scale, landmark)
@@ -122,10 +149,46 @@ def main(db_path, model_name, distance_metric, face_frame=5):
 
 						current_faces.append(detected_face)
 						face_included_frames += 1
+			elif len(faces) != 1:
+				face_included_frames = 0
+				have_face = False
+				current_faces = []
+				
+				if identity != None:
+					_prev_time = time.time()
+					_current_time = _prev_time
+					card_height = card_min
+					while _current_time - _prev_time <= delay:
+						_frames = pipeline.wait_for_frames()
+						_color_frame = _frames.get_color_frame()
+						_color_image = np.asanyarray(_color_frame.get_data())
+						
+						# animate alpha
+						if _current_time - _prev_time > delay - alpha_animate_frame * 1/fps:
+							alpha += alpha < max_alpha and alpha_animate_amount
+						
+						#animate card
+						if _current_time - _prev_time > delay - card_animate_frame * 1/fps:
+							card_height += card_height < card_max and card_animate_amount
+
+						_color_image = addUserCard(_color_image, alpha, card_height, card_top_left, card_bottom_right, bg_color, fg_color, identity['identity'], identity['time'], font_path, font_size)
+
+						cv2.imshow('frame', _color_image)
+						if cv2.waitKey(1) & 0xFF == ord('q'):
+							break
+
+						_current_time = time.time()
+
+					identity = None
+					continue
 					
-					if identity != None:
-						#TODO: Add UI to indicate user sign in
-						cv2.putText(color_image, identity['identity'], (l,t), cv2.FONT_HERSHEY_SIMPLEX, 0.5, text_color, 1)
+			if identity != None:
+				#TODO: animate alpha and position
+				alpha -= alpha > min_alpha and alpha_animate_amount
+				card_height -= card_height > card_min and card_animate_amount
+
+				#cv2.putText(color_image, identity['identity'], (l,t), cv2.FONT_HERSHEY_SIMPLEX, 0.5, text_color, 1)
+				color_image = addUserCard(color_image, alpha, card_height, card_top_left, card_bottom_right, bg_color, fg_color, identity['identity'], identity['time'], font_path, font_size)
 
 			# Face recognition
 			if face_included_frames >= face_frame and not have_face:
@@ -138,7 +201,7 @@ def main(db_path, model_name, distance_metric, face_frame=5):
 			if cv2.waitKey(1) & 0xFF == ord('q'):
 				break
 	except Exception as e:
-		print(str(e))
+		print(e)
 	finally:
 		cv2.destroyAllWindows()
 		pipeline.stop()
--- python/utils/user_card.py
+++ python/utils/user_card.py
@@ -8,6 +8,7 @@ from PIL import Image, ImageFont, ImageDraw
 user_img_offset = (60, 15)
 text_offset = (40,10)
 text_padding = 5
+cornerRadius = 10
 
 class Point:
     def __init__(self, x: int, y: int):
@@ -17,11 +18,17 @@ class Point:
     def __add__(self, other: Point) -> Point:
         return Point(self.x + other.x, self.y + other.y)
 
+    def __sub__(self, other: Point) -> Point:
+        return Point(self.x - other.x, self.y - other.y)
+
     def toTuple(self) -> Tuple:
         return (self.x, self.y)
+    
+    def fromTuple(coord: tuple) -> Point:
+        return Point(coord[0], coord[1])
 
 
-def rounded_rectangle(src: np.array, top_left: tuple, bottom_right: tuple, cornerRadius: int = 10, color: tuple = (255,255,255), thickness: int = 1, lineType: int=cv2.LINE_AA) -> Any:
+def rounded_rectangle(src: np.array, top_left: tuple, bottom_right: tuple, cornerRadius: int = cornerRadius, color: tuple = (255,255,255), thickness: int = 1, lineType: int=cv2.LINE_AA) -> Any:
     """Create a rounded rectangle
 
     Args:
@@ -63,8 +70,93 @@ def rounded_rectangle(src: np.array, top_left: tuple, bottom_right: tuple, corne
     cv2.ellipse(src, (p3+Point(-cornerRadius, -cornerRadius)).toTuple(), (cornerRadius, cornerRadius), 0.0, 0, 90, color, thickness, lineType);
     cv2.ellipse(src, (p4+Point(cornerRadius, -cornerRadius)).toTuple(), (cornerRadius, cornerRadius), 90.0, 0, 90, color, thickness, lineType);
 
+def _userCard(top_left: tuple, bottom_right: tuple, background_color: tuple, foreground_color: tuple, identity: str, time: datetime, font_path: str, font_size: int=24) -> np.array:
+    """Generate a user card
 
-def userCard(src: np.array, alpha: float, top_left: tuple, bottom_right: tuple, background_color: tuple, foreground_color: tuple, identity: str, time: datetime, font_path: str, font_size: int=24) -> np.array:
+    Args:
+        src (np.array): the source (non-destructive).
+        alpha (float): the amount that the picture will be dimmed [0.0-1.0].
+        top_left (tuple): The top left vertex of the rectangle.
+        bottom_right (tuple): The bottom right vertex of the rectangle.
+        background_color (tuple): background color.
+        foreground_color (tuple): forground color.
+        identity (str): the identity of the user.
+        time (datetime): time that user scans their face.
+        font_path (str): the path to the font.
+        font_size (int): font size. Defaults to 24.
+
+    Returns:
+        np.array: a new img that has the card overlay on top
+    """
+
+    card_top_left = (0,0)
+    card_bottom_right = (Point.fromTuple(coord=bottom_right) - Point.fromTuple(coord=top_left)).toTuple()
+    
+    card = np.zeros((card_bottom_right[1], card_bottom_right[0], 3), dtype=np.dtype('uint8'))
+
+    # add background
+    rounded_rectangle(card, card_top_left, card_bottom_right, color=background_color, thickness=-1)
+    mask = card.copy()
+
+    # add the user image to the card
+    user_img_top_left = (card_top_left[1] + user_img_offset[1], card_top_left[0] + user_img_offset[0])
+    user_img_path = f'face_pic/{identity}.jpg'
+
+    user_img = cv2.imread(user_img_path)
+    user_img = cv2.resize(user_img, (round(user_img.shape[1]*.25), round(user_img.shape[0]*.25)))
+
+    user_img_size = user_img.shape
+    
+    card[user_img_top_left[0]:user_img_top_left[0]+user_img_size[0], user_img_top_left[1]:user_img_top_left[1]+user_img_size[1]] = user_img
+
+    # add text
+    text_top_left = (user_img_top_left[1]+user_img_size[1]+text_offset[0], user_img_top_left[0]+text_offset[1])
+    
+    time_str = time.strftime('%H:%M:%S')
+
+    font = ImageFont.truetype(font_path, font_size)
+
+    card_pil = Image.fromarray(card)
+    draw = ImageDraw.Draw(card_pil)
+    
+    draw.text(text_top_left, f'เลขประจำตัว: {identity}', font=font, fill=foreground_color)
+    draw.text((text_top_left[0], text_top_left[1] + font_size + text_offset[1] + text_padding), f'เวลา: {time_str}', font=font, fill=foreground_color)
+
+    card = np.array(card_pil)
+    
+    return card, mask
+
+def overlayCard(src: np.array, card: np.array, mask:np.array, top_left: Point) -> Any:
+    """Overlay the card on top of the src
+
+    Args:
+        src (np.array): the source img
+        card (np.array): the card img
+        mask (np.array): the mask of card
+        top_left (Point): the top left point where  the card will go
+    """
+    
+    max_y = src.shape[0]
+    # if top left is outside the src
+    if top_left.y >= max_y:
+        return
+
+    # get the ROI
+    rows, cols, _ = mask.shape
+    roi = src[top_left.y:max_y, top_left.x:top_left.x+cols]
+    roi_shape = roi.shape
+
+    mask_grey = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    _, mask = cv2.threshold(mask_grey, 127, 255, cv2.THRESH_BINARY)
+    mask_inv = cv2.bitwise_not(mask)
+
+    img_bg = cv2.bitwise_and(roi, roi, mask=mask_inv[:roi_shape[0], :roi_shape[1]])
+    card_fg = cv2.bitwise_and(card, card, mask=mask)
+
+    dst = cv2.add(img_bg, card_fg[:roi_shape[0], :roi_shape[1]])
+    src[top_left.y:max_y, top_left.x:top_left.x+cols] = dst
+
+def addUserCard(src: np.array, alpha: float, current_y: float, top_left: tuple, bottom_right: tuple, background_color: tuple, foreground_color: tuple, identity: str, time: datetime, font_path: str, font_size: int=24) -> np.array:
     """Generate a user card to overlay on top of the src
 
     Args:
@@ -88,33 +180,10 @@ def userCard(src: np.array, alpha: float, top_left: tuple, bottom_right: tuple, 
     # dimmed the source
     img = cv2.addWeighted(img, alpha, np.zeros(img.shape, img.dtype), 1 - alpha, 0)
     
-    # add background
-    rounded_rectangle(img, top_left, bottom_right, color=background_color, thickness=-1)
+    # get the user card
+    card, mask = _userCard(top_left, bottom_right, background_color, foreground_color, identity, time, font_path, font_size)
 
-    # add the user image to the card
-    user_img_top_left = (top_left[1] + user_img_offset[1], top_left[0] + user_img_offset[0])
-    user_img_path = f'face_pic/{identity}.jpg'
-
-    user_img = cv2.imread(user_img_path)
-    user_img = cv2.resize(user_img, (round(user_img.shape[1]*.25), round(user_img.shape[0]*.25)))
-
-    user_img_size = user_img.shape
-    
-    img[user_img_top_left[0]:user_img_top_left[0]+user_img_size[0], user_img_top_left[1]:user_img_top_left[1]+user_img_size[1]] = user_img
-
-    # add text
-    text_top_left = (user_img_top_left[1]+user_img_size[1]+text_offset[0], user_img_top_left[0]+text_offset[1])
-    
-    time_str = time.strftime('%H:%M:%S')
-
-    font = ImageFont.truetype(font_path, font_size)
-
-    img_pil = Image.fromarray(img)
-    draw = ImageDraw.Draw(img_pil)
-    
-    draw.text(text_top_left, f'เลขประจำตัว: {identity}', font=font, fill=foreground_color)
-    draw.text((text_top_left[0], text_top_left[1] + font_size + text_offset[1] + text_padding), f'เวลา: {time_str}', font=font, fill=foreground_color)
-    
-    img = np.array(img_pil)
+    # overlay card
+    overlayCard(img, card, mask, Point(top_left[0], int(current_y)))
 
     return img 
